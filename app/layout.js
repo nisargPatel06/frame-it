ca7959614bc0c0b6cee3e6786cb57352e458afda
Nisarg Patel <nisargpatel6504@gmail.com>
2025-10-14 01:06:17 +0530

    Basic setup with shadcn ui..

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
+import { FloatingShapes } from "@/components/floating-shapes";
+import Header from "@/components/header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -18,11 +14,22 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className}`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {/* <Header /> */}
+          <main className="bg-slate-900 min-h-screen text-white overflow-x-hidden">
+            <FloatingShapes />
+            <Toaster richColors />
+
+            {children}
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
